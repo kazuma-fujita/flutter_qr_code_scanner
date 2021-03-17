067272e3be53184f848cf590a6ab7fc684248aae
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-03-17 18:58:48 +0900

    refactoring

--- lib/qr_code_scanner_view.dart
+++ lib/qr_code_scanner_view.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'first_page_view.dart';
 
 @immutable
